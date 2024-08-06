6eadfe51b23a78045140ccbda3e8fbd40cb0bfb8
MANUEL <61389754+ManuelSystem@users.noreply.github.com>
2024-08-06 09:59:37 -0500

    Update roleMiddleware.js

--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,8 +1,8 @@
 const roleMiddleware = (roles) => (req, res, next) => {
-    if (!roles.include(req.user.role)) {
-        return res.status(403).json({ msg: 'Acceso denegado, no tiene un rol'});
+    if (!roles.includes(req.user.role)) {
+        return res.status(403).json({ msg: 'Acceso denegado: no tiene el rol adecuado' });
     }
     next();
-}
+};
 
 module.exports = roleMiddleware;
